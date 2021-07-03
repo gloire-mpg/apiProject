fb9adc6fd2a31959cf19c38aa65383810557de3e
gloire-mpg <69449719+gloire-mpg@users.noreply.github.com>
2021-07-03 17:25:58 +0530

    Updates

---
+++ precache-manifest.49c5e1dfd65943f181fa4aa1b0115f09.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c4b32b5a22773d6133b74ed546ae266",
+    "url": "/apiProject/index.html"
+  },
+  {
+    "revision": "1af19f94f5118ec16c2e",
+    "url": "/apiProject/static/css/main.4d04be1c.chunk.css"
+  },
+  {
+    "revision": "91a7d88f2955c7b43756",
+    "url": "/apiProject/static/js/2.fbc8c138.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/apiProject/static/js/2.fbc8c138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1af19f94f5118ec16c2e",
+    "url": "/apiProject/static/js/main.16c90900.chunk.js"
+  },
+  {
+    "revision": "d232b5e6d74d3841b524",
+    "url": "/apiProject/static/js/runtime-main.bb7312b7.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/apiProject/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/apiProject/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/apiProject/static/media/spinner.235b9b3e.gif"
+  }
+]);
